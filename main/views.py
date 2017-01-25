f0580149dc19941f39f18699f7cf1c910ed1dca7
tux <bitsharp@gmail.com>
2017-01-25 14:43:14 +0900

    index page created

--- main/views.py
+++ main/views.py
@@ -1,18 +1,48 @@
-from django.shortcuts import render
-from student.models import StudentProfile
-from django.shortcuts import render, HttpResponseRedirect, redirect
-from django.utils import timezone
-from django.db import IntegrityError
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
-from django.contrib import auth
-from django.utils import timezone
-from PIL      import Image
-import datetime
-import json
-import StringIO
-import threading
+# coding=utf-8
+
+from django.http                    import HttpResponse, HttpResponseRedirect
+from django.shortcuts               import render, get_object_or_404
+from django.contrib                 import auth
+from django.contrib.auth.decorators import login_required
+from django.core.mail               import send_mass_mail
+from django.db                      import IntegrityError
+
+from main.models    import *
+from student.models import *
+from teacher.models import *
+
+
+### helpers
+
+def login(request, login, password):
+    user = auth.authenticate(username = login, password = password)
+    if user != None and user.is_active:
+        auth.login(request, user)
+
+        return user
+
+    return None
+
+### request handlers
 
 def index(request):
-    return render(request,"main/index.html")
+    has_error     = False
+    error_message = ""
+
+    authorized = request.user.is_authenticated()
+
+    if authorized:
+        return HttpResponseRedirect("/" + request.user.profile.username + "/")
+
+    if request.method == "POST" and "sign_in" in request.POST:
+        login    = request.POST['login']
+        password = request.POST['password']
+
+        user = login(request, login, password)
+        if user:
+            return HttpResponseRedirect("/" + request.user.profile.username + "/")
+        else:
+            has_error     = True
+            error_message = "Не удалось войти в систему"
+
+    return render(request, 'main/index.html', locals())
