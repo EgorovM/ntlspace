a9165332065a34fad57081de102170cd3d840604
EgorovM <egorov_michil@mail.ru>
2017-01-28 12:20:13 +0900

    переход в страницу профиля

--- student/views.py
+++ student/views.py
@@ -8,26 +8,35 @@ from django.contrib.auth        import authenticate
 from django.contrib             import auth
 from PIL                        import Image
 
-from .models import *
-from .forms  import *
+from .models    import *
+from .forms     import *
+from main.views import sign_in, redirect_to_profile
 
  
 def register(request):
-    false_message = ""
+    error_message = ""
     user_form    = UserForm()
     profile_form = StudentProfileForm()
     
     if request.method == "POST":
-		user_form    = UserForm(request.POST)
-		profile_form = StudentProfileForm(request.POST)
+        user_form    = UserForm(request.POST)
+        profile_form = StudentProfileForm(request.POST,request.FILES)
 
-		if user_form.is_valid() and profile_form.is_valid():
-			user    = user_form.save()
-			profile = profile_form.save(commit = False)
+        if user_form.is_valid() and profile_form.is_valid():
+            user    = user_form.save()
+            profile = profile_form.save(commit = False)
 
-			profile.user = user
-			profile.save()
+            profile.user = user
+            profile.save()
 
+            username = user_form["username"].value()
+            password = user_form["password"].value()
+
+            user = sign_in(request, username, password)
+
+            if user:
+                return redirect_to_profile(user)
+       
     return render(request,'student/register.html',locals())
     
 def profile(request, nick):
@@ -41,7 +50,6 @@ def profile(request, nick):
     
     if authorized:
         profile = StudentProfile.objects.get(user=user)      
-        context = {"profile":profile,}
     
-    return render(request,'student/profile.html',context)
+    return render(request, 'student/profile.html', locals())
     
