b0e66cc7a2063a32e2366240da606b76f3ec70da
tux <bitsharp@gmail.com>
2017-01-28 13:33:22 +0900

    labels for teacher registration form

--- teacher/forms.py
+++ teacher/forms.py
@@ -10,3 +10,17 @@ class TeacherProfileForm(BootstrapModelForm):
     class Meta:
         model  = TeacherProfile
         fields = ['name', 'sex', 'birthdate', 'avatar', 'position', 'qualification', 'subject', 'education', 'educational_institution', 'work_experience', 'teaching_experience']
+
+        labels = {
+            "name"                   : "Полное имя",
+            "sex"                    : "Пол",
+            "birthdate"              : "Дата рождения",
+            "avatar"                 : "Аватар",
+            "position"               : "Должность",
+            "qualification"          : "Квалификация",
+            "subject"                : "Предмет",
+            "education"              : "Образование",
+            "educational_institution": "Образовательное учреждение",
+            "work_experience"        : "Общий стаж",
+            "teaching_experience"    : "Педагогический стаж",
+        }
